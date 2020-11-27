f55b01f36c6d9eef138c3b7ae1bfd414e1747d5d
Saeed Rasooli <saeed.gnu@gmail.com>
2020-11-28 00:30:08 +0330

    WIP: ui_wx

--- pyglossary/ui/main.py
+++ pyglossary/ui/main.py
@@ -194,6 +194,7 @@ def main():
 			"cmd",
 			"gtk",
 			"tk",
+			"wx",
 			# "qt",
 			"auto",
 			"none",
@@ -222,6 +223,14 @@ def main():
 		const="tk",
 		default=None,
 		help="use Tkinter-based user interface",
+	)
+	parser.add_argument(
+		"--wx",
+		dest="ui_type",
+		action="store_const",
+		const="wx",
+		default=None,
+		help="use wxWidgets/wxPython user interface",
 	)
 	parser.add_argument(
 		"--interactive",
@@ -397,6 +406,7 @@ def main():
 	ui_list = [
 		"gtk",
 		"tk",
+		"wx",
 	]
 
 	# log.info(f"PyGlossary {core.VERSION}")
